0b19cf2cf1f065e1b1e5c4f847ec17b990e0fd07
servelcero-cmd <servelcero@gmail.com>
2025-08-19 13:32:02 -0400

    Create tests.py

---
+++ backend/api/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# En este archivo se escriben las pruebas para asegurar la calidad del código.
+class APITests(TestCase):
+
+    def test_placeholder(self):
+        """
+        Una prueba inicial simple para asegurar que el sistema de tests funciona.
+        """
+        self.assertEqual(1 + 1, 2)
